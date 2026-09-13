625646ec6163f79b6f23389dcddb4ac921ed4924
Ibragim <hehehdfhehhe@gmail.com>
2026-09-13 18:52:20 +0500

    firs

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,4 @@
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
+import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import path from "path"
 import tailwindcss from "@tailwindcss/vite"
